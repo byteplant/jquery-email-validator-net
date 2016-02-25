212d19abf4b360c3a2eea1119ccc797dcaae89e0
unknown <unknown@unknown.com>
2016-02-25 14:03:37 +0100

    lil changes

--- jquery.email-validator-net.js
+++ jquery.email-validator-net.js
@@ -9,16 +9,16 @@
       $.get(baseUrl + "?EmailAddress=" + email + "&APIKey=" + apiKey, $.proxy(function (res) {
         if (/2[0-9]{2}/g.test(res.status)) {
           this.css('border-color', 'green');
-          res['easyStatus'] = "VALID"
+          res['simpleStatus'] = "VALID"
         } else if (/3[0-9]{2}/g.test(res.status)) {
           this.css('border-color', 'orange');
-          res['easyStatus'] = "SUSPECT"
+          res['simpleStatus'] = "SUSPECT"
         } else if (/4[0-9]{2}/g.test(res.status)) {
           this.css('border-color', 'red');
-          res['easyStatus'] = "INVALID"
+          res['simpleStatus'] = "INVALID"
         } else if (/1[0-9]{2}/g.test(res.status)) {
           this.css('border-color', 'black');
-          res['easyStatus'] = "INDETERMINATE"
+          res['simpleStatus'] = "INDETERMINATE"
         }
         cb(res)
       }, this));
--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 $( document ).ready(function() {
 
   // Init only once
-  $.validateEmail("ev-35f3931d458da40d5a3349e5498bb800");
+  $.validateEmail("YOUR API KEY");
 
   // OnClick
   $("#submit").click(function () {
